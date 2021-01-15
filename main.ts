bc938c0889be09d3ab9abb716891d816328ebe05
Qcd1587 <77481111+Qcd1587@users.noreply.github.com>
2021-01-15 14:28:49 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . # . #
+    # # # # #
+    `)
+basic.showIcon(IconNames.No)
+basic.pause(500)
+basic.clearScreen()
+basic.showString("GOOD")
+basic.clearScreen()
+basic.showString("Nithiwadee")
+basic.clearScreen()
+basic.showNumber(10)
+basic.clearScreen()
